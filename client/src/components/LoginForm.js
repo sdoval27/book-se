5b3fddf13eae1d7a252480b85a05a6ea62617140
unknown <sashardoval@gmail.com>
2023-05-18 17:43:31 -0700

    login form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,13 +2,24 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [login, {error}] = useMutation(LOGIN_USER);
+
+  useEffect(() => {
+    if(error) {
+      setShowAlert(true);
+    }else {
+      setShowAlert(false);
+    }
+  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,7 +37,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await LOGIN_USER(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
