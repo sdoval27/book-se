b34adc23cd757f3698b32145926303021675ee93
unknown <sashardoval@gmail.com>
2023-05-18 18:01:37 -0700

    signup form

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { CREATE_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +12,16 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [createUser, {error}] = useMutation(CREATE_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    }else{
+      setShowAlert(false);
+    }
+  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,19 +39,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
+      const {data} = await createUser ({
+        variables: {...userFormData},
+      });
+    
+    console.log (data);
+    Auth.login(data.createUser.token);
+  } catch (err) {
+    console.error(err);
+  }
 
     setUserFormData({
       username: '',
